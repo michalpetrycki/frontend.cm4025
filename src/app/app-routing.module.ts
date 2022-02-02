3308591eaba2c9a68386ba15fcc5e7e7b096ceff
Michal <michalpetrycki@gmail.com>
2022-02-02 23:04:29 +0000

    Addition of missing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'admin', component: AdminComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { AdminComponent } from './components/admin/admin/admin.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/components/app/app.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { AdminComponent } from 'src/app/components/admin/admin.component';
+import { UsersComponent } from 'src/app/components/users/users.component';
+import { PostsComponent } from 'src/app/components/posts/posts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AdminComponent
+    AdminComponent,
+    UsersComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
