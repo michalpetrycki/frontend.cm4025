5e9e07b38d1d9c09d4b77bfa01acf2784091d1f3
Michal <michalpetrycki@gmail.com>
2022-02-03 20:56:21 +0000

    Add login and register routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
 import { PostsComponent } from './components/posts/posts.component';
+import { RegisterComponent } from './components/register/register.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
 
   { path: 'admin', component: AdminComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'posts', component: PostsComponent }
+  { path: 'posts', component: PostsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 
 ];
 
